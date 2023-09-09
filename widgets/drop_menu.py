002f0384760d3dba7d0dca85aa130a94beb33d45
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-09-09 21:44:59 +0530

    menu

--- widgets/drop_menu.py
+++ widgets/drop_menu.py
@@ -3,7 +3,12 @@ from PIL import ImageTk,Image
 
 root = Tk()
 root.title("Dropdown Menu")
+root.geometry("400x400")
 
+click = StringVar()
+click.set("Pizza")
 
+drop = OptionMenu(root, click, "Pizza", "Burgur", "fries", "soft Drink")
+drop.pack()
 
 root.mainloop()
