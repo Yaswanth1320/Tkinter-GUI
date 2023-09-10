0804e0b3d5ab4b974bd2758acfdefc6af76d88a9
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-09-10 09:44:47 +0530

    dropdown

--- widgets/drop_menu.py
+++ widgets/drop_menu.py
@@ -5,10 +5,24 @@ root = Tk()
 root.title("Dropdown Menu")
 root.geometry("400x400")
 
-click = StringVar()
-click.set("Pizza")
+def display():
+    label = Label(root, text=click.get()).pack()
 
-drop = OptionMenu(root, click, "Pizza", "Burgur", "fries", "soft Drink")
+option_list = [
+    "Pizza", 
+    "Burgur", 
+    "fries", 
+    "soft Drink",
+    "coffee",
+    "ice cream"
+]
+
+click = StringVar()
+click.set(option_list[0])
+
+drop = OptionMenu(root, click, *option_list)
 drop.pack()
+
+my_btn = Button(root, text="Select the item", command=display).pack()
 
 root.mainloop()
