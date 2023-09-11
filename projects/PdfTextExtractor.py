64c1afb3a16c54258d5c2fedfdbacf54270dced6
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-09-11 21:47:27 +0530

    File exctrator

--- projects/PdfTextExtractor.py
+++ projects/PdfTextExtractor.py
@@ -1,12 +1,18 @@
 import tkinter
-import pyPDf2
+import PyPDf2
 from tkinter import filedialog
-
 
 def openfile():
     filename = filedialog.askopenfilename(title="Open Pdf file",
                                           initialdir="/Users/yaswanth/Python:Tkinter/projects",
                                           filetypes=[('Pdf files', '*.pdf')])
+    
+    filename_label.configure(text=filename)
+    extractor_text.delete("1.0", tkinter.END)
+    reader = PyPDf2.PdfReader(filename)
+    for i in range(reader.numPages):
+            current_text = reader.getPage(i).extractText()
+            extractor_text.insert(tkinter.END, current_text)
 
 root = tkinter.Tk()
 root.title("Pdf Text Extractor")
