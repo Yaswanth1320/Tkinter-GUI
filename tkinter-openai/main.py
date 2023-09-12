e22e39c84efca612c2aa8959a21ce33c2c2535fc
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-09-12 16:40:19 +0530

    env

--- tkinter-openai/main.py
+++ tkinter-openai/main.py
@@ -1,6 +1,6 @@
 import os
 import openai 
-import customtkinter as ctk 
+import customtkinter as ctk
 
 def generate():
     prompt = "Please generate 10 ideas for coding projects. "
@@ -18,7 +18,7 @@ def generate():
     
     print(prompt)
     
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = os.getenv("API_KEY")
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
